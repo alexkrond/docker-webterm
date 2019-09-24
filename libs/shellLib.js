c94eb95b5416347968a1722d53ad524b1128ff27
Alex Kashko <alexkrond@yahoo.com>
2019-09-24 13:24:11 +0300

    При создании терминала с процессом докера в аргументы добавляется текущий докер хост

--- libs/shellLib.js
+++ libs/shellLib.js
@@ -1,7 +1,12 @@
 const pty = require("node-pty");
+const dockerHosts = require("../dockerHost.config.js");
 
 
 function getShell(file, args = []) {
+  if (file.indexOf("docker") !== -1 && dockerHosts.current !== dockerHosts.hosts["localhost"]) {
+    args.unshift("-H", dockerHosts.current);
+  }
+
   const shell = pty.spawn(file, args, {
     name: 'xterm-color',
     cwd: process.env.PWD,
