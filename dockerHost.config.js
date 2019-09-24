26cd2c06760679f8afac4594790399eb5de86f7a
Alex Kashko <alexkrond@yahoo.com>
2019-09-24 18:18:58 +0300

    ISSUE-7 Заккоментированы хосты

--- dockerHost.config.js
+++ dockerHost.config.js
@@ -5,13 +5,13 @@ module.exports = {
       name: "localhost",
       url: ""
     },
-    "test-machine-1": {
-      name: "test-machine-1",
-      url: "tcp://192.168.33.10:2375"
-    },
-    "test-machine-2": {
-      name: "test-machine-2",
-      url: "tcp://192.168.44.10:2375"
-    }
+    // "test-machine-1": {
+    //   name: "test-machine-1",
+    //   url: "tcp://192.168.33.10:2375"
+    // },
+    // "test-machine-2": {
+    //   name: "test-machine-2",
+    //   url: "tcp://192.168.44.10:2375"
+    // }
   }
 };
