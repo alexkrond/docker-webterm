ef0a7cf1d1697f05f435d8138d4c5ea2955a8f61
Alex Kashko <alexkrond@yahoo.com>
2019-09-25 15:45:53 +0300

    ISSUE-13 Изменена конфигурация файла dockerHost.config.js

--- dockerHost.config.js
+++ dockerHost.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-  current: null,
   hosts: {
     "localhost": {
-      name: "localhost",
+      name: "Локальная машина",
       url: ""
     },
     // "test-machine-1": {
-    //   name: "test-machine-1",
+    //   name: "Тестовая машина №1",
     //   url: "tcp://192.168.33.10:2375"
     // },
     // "test-machine-2": {
-    //   name: "test-machine-2",
+    //   name: "Тестовая машина №2",
     //   url: "tcp://192.168.44.10:2375"
     // }
   }
